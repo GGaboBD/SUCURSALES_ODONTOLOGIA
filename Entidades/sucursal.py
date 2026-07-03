44ad1aa4ae3e53cbd3e5c977aee8976725c9c66d
GGaboBD <007007gabo7u7@gmail.com>
2026-07-03 13:45:08 -0600

    COMMIT 2: agregacion de entidades ventas y sucursal

---
+++ Entidades/sucursal.py
@@ -0,0 +1,6 @@
+class Sucursal:
+    def __init__(self, ID_sucursal: int, nombre_sucursal: str, direccion_sucursal: str, telefono_sucursal: str):
+        self.ID_sucursal = ID_sucursal
+        self.nombre_sucursal = nombre_sucursal
+        self.direccion_sucursal = direccion_sucursal
+        self.telefono_sucursal = telefono_sucursal
---
+++ Entidades/ventas.py
@@ -0,0 +1,6 @@
+class Ventas:
+    def __init__(self, ID_ventas: int, fecha_ventas: str, monto_venta: float, ID_sucursal: int):
+        self.ID_ventas = ID_ventas
+        self.fecha_ventas = fecha_ventas
+        self.monto_venta = monto_venta
+        self.ID_sucursal = ID_sucursal
